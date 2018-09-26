b432a462b70bc3b960672badf6498f7ef3549408
rifkegribenes <rifkegribenes@gmail.com>
2018-09-26 03:41:20 +0000

    bcrypt challenges

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express     = require('express');
 const bodyParser  = require('body-parser');
 const fccTesting  = require('./freeCodeCamp/fcctesting.js');
 const app         = express();
+const bcrypt      = require('bcrypt');
 
 fccTesting(app); //For FCC testing purposes
 
@@ -13,7 +14,10 @@ const someOtherPlaintextPassword = 'pass123';
 
 //START_ASYNC -do not remove notes, place code between correct pair of notes.
 
-
+bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash) => { 
+  console.log(hash); //$2a$12$Y.PHPE15wR25qrrtgGkiYe2sXo98cjuMCG1YwSI5rJW1DSJp0gEYS
+  bcrypt.compare(myPlaintextPassword, hash, (err, res) => { /*res == true or false*/ });
+});
 
 //END_ASYNC
 
